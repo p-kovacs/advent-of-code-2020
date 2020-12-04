5f311a6c217c7e3e084dd729db217ba5a21e8c78
Péter Kovács <kovacspeter84@gmail.com>
2020-12-04 10:14:21 +0100

    Add solution for Day 1

---
+++ src/main/java/pkovacs/aoc/Day01.java
@@ -0,0 +1,37 @@
+package pkovacs.aoc;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day01 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(Day01.class.getResourceAsStream("day01.txt"));
+        var list = new ArrayList<Integer>();
+        while (in.hasNext()) {
+            list.add(in.nextInt());
+        }
+
+        int solution1 = 0;
+        int solution2 = 0;
+        for (int i = 0; i < list.size(); i++) {
+            int x = list.get(i);
+            for (int j = i + 1; j < list.size(); j++) {
+                int y = list.get(j);
+                if (x + y == 2020) {
+                    solution1 = x * y;
+                }
+                for (int k = j + 1; k < list.size(); k++) {
+                    int z = list.get(k);
+                    if (x + y + z == 2020) {
+                        solution2 = x * y * z;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Puzzle 1: " + solution1);
+        System.out.println("Puzzle 2: " + solution2);
+    }
+
+}
