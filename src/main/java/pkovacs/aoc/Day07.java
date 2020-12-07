00daf5a1f49399df6fc93a7a8fcf5ecd4774a275
Péter Kovács <kovacspeter84@gmail.com>
2020-12-07 16:56:09 +0100

    Add another solution for Day 7

--- src/main/java/pkovacs/aoc/Day07.java
+++ src/main/java/pkovacs/aoc/Day07.java
@@ -3,8 +3,10 @@ package pkovacs.aoc;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import pkovacs.aoc.util.AocUtils;
 
@@ -12,7 +14,14 @@ public class Day07 {
 
     public static void main(String[] args) {
         List<String> lines = AocUtils.readLines("day07.txt");
+        runSolutionA(lines);
+        runSolutionB(lines);
+    }
 
+    /**
+     * My original approach (after some refactoring).
+     */
+    private static void runSolutionA(List<String> lines) {
         var graph = new HashMap<String, List<String>>();
         var revGraph = new HashMap<String, List<String>>();
         var count = new HashMap<String, Map<String, Integer>>();
@@ -64,6 +73,59 @@ public class Day07 {
         }
 
         return results.subList(1, results.size()); // skip first path (to source)
+    }
+
+    /**
+     * An alternative approach, influenced by the solution of a co-worker.
+     */
+    private static void runSolutionB(List<String> lines) {
+        var bags = new HashMap<String, Bag>();
+        for (var line : lines) {
+            var parts = AocUtils.scan(line, "%s bag.*contain %s");
+            var color = parts.get(0).get();
+            var bag = bags.computeIfAbsent(color, c -> new Bag(c));
+            var contents = parts.get(1).get();
+            if (!contents.startsWith("no other")) {
+                for (var content : contents.split(",")) {
+                    var values = AocUtils.scan(content, " *%d %s bag.*");
+                    var childColor = values.get(1).get();
+                    var childBag = bags.computeIfAbsent(childColor, c -> new Bag(c));
+                    bag.children.add(childBag);
+                    bag.childrenCount.put(childColor, values.get(0).asInt());
+                    childBag.parents.add(bag);
+                }
+            }
+        }
+
+        var s = "shiny gold";
+        System.out.println("Puzzle 1: " + bags.get(s).collectAncestorColors().size());
+        System.out.println("Puzzle 2: " + bags.get(s).countDescendants());
+    }
+
+    private static class Bag {
+
+        final String color;
+        final List<Bag> parents = new ArrayList<>();
+        final List<Bag> children = new ArrayList<>();
+        final Map<String, Integer> childrenCount = new HashMap<>();
+
+        Bag(String color) {
+            this.color = color;
+        }
+
+        Set<String> collectAncestorColors() {
+            var result = new HashSet<String>();
+            parents.forEach(b -> {
+                result.add(b.color);
+                result.addAll(b.collectAncestorColors());
+            });
+            return result;
+        }
+
+        long countDescendants() {
+            return children.stream().mapToLong(c -> childrenCount.get(c.color) * (1 + c.countDescendants())).sum();
+        }
+
     }
 
 }
