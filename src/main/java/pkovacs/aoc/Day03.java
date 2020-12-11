959891b15bb37bc95dd65400058f83b1e8d05e4b
Péter Kovács <kovacspeter84@gmail.com>
2020-12-11 07:03:32 +0100

    Refactor Day03 using the new util methods

--- src/main/java/pkovacs/aoc/Day03.java
+++ src/main/java/pkovacs/aoc/Day03.java
@@ -7,23 +7,23 @@ import pkovacs.aoc.util.AocUtils;
 public class Day03 {
 
     public static void main(String[] args) {
-        List<String> map = AocUtils.readLines("day03.txt");
+        char[][] map = AocUtils.readCharMatrix("day03.txt");
 
         System.out.println("Puzzle 1: " + solve(map, new int[] { 3 }, new int[] { 1 }));
         System.out.println("Puzzle 2: " + solve(map, new int[] { 1, 3, 5, 7, 1 }, new int[] { 1, 1, 1, 1, 2 }));
     }
 
-    private static long solve(List<String> map, int[] right, int[] down) {
+    private static long solve(char[][] map, int[] right, int[] down) {
         long result = 1;
         for (int i = 0; i < right.length; i++) {
-            int x = 0, y = 0;
+            int c = 0, r = 0;
             long cnt = 0;
-            while (y < map.size()) {
-                if (map.get(y).charAt(x) == '#') {
+            while (r < map.length) {
+                if (map[r][c] == '#') {
                     cnt++;
                 }
-                x = (x + right[i]) % map.get(y).length();
-                y += down[i];
+                c = (c + right[i]) % map[r].length;
+                r += down[i];
             }
             result *= cnt;
         }
