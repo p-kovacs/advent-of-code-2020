2b93e42ce58143c99593d6b3dddb9b4574afcc99
Péter Kovács <kovacspeter84@gmail.com>
2020-12-12 17:33:50 +0100

    Use new util class for Day12

--- src/main/java/pkovacs/aoc/Day12.java
+++ src/main/java/pkovacs/aoc/Day12.java
@@ -3,72 +3,61 @@ package pkovacs.aoc;
 import java.util.List;
 
 import pkovacs.aoc.util.AocUtils;
+import pkovacs.aoc.util.Vector;
 
 public class Day12 {
 
     public static void main(String[] args) {
         List<String> lines = AocUtils.readLines("day12.txt");
 
-        System.out.println("Puzzle 1: " + solvePuzzle1(lines));
-        System.out.println("Puzzle 2: " + solvePuzzle2(lines));
+        System.out.println("Puzzle 1: " + solvePuzzle(lines, false));
+        System.out.println("Puzzle 2: " + solvePuzzle(lines, true));
     }
 
-    private static long solvePuzzle1(List<String> lines) {
-        long x = 0, y = 0;
-        long wx = 1, wy = 0;
+    private static long solvePuzzle(List<String> lines, boolean second) {
+        var ship = new Position(0, 0);
+        var waypoint = second ? new Position(10, 1) : new Position(1, 0);
         for (var line : lines) {
             char ch = line.charAt(0);
-            var v = Long.parseLong(line.substring(1));
+            int v = Integer.parseInt(line.substring(1));
             if (ch == 'F') {
-                x += wx * v;
-                y += wy * v;
-            } else if (ch == 'N') {
-                y -= v;
-            } else if (ch == 'S') {
-                y += v;
-            } else if (ch == 'E') {
-                x += v;
-            } else if (ch == 'W') {
-                x -= v;
+                ship.add(waypoint.x * v, waypoint.y * v);
+            } else if (ch == 'N' || ch == 'S' || ch == 'E' || ch == 'W') {
+                (second ? waypoint : ship).move(ch, v);
             } else if (ch == 'L' || ch == 'R') {
-                int rightTurns = (int) (ch == 'R' ? v : 360 - v) / 90;
-                for (int i = 0; i < rightTurns; i++) {
-                    long oldx = wx;
-                    wx = -1 * wy;
-                    wy = oldx;
-                }
+                waypoint.rotate(ch, v);
             }
         }
-        return Math.abs(x) + Math.abs(y);
+        return ship.length();
     }
 
-    private static long solvePuzzle2(List<String> lines) {
-        long x = 0, y = 0;
-        long wx = 10, wy = -1;
-        for (var line : lines) {
-            char ch = line.charAt(0);
-            var v = Long.parseLong(line.substring(1));
-            if (ch == 'F') {
-                x += wx * v;
-                y += wy * v;
-            } else if (ch == 'N') {
-                wy -= v;
-            } else if (ch == 'S') {
-                wy += v;
-            } else if (ch == 'E') {
-                wx += v;
-            } else if (ch == 'W') {
-                wx -= v;
-            } else if (ch == 'L' || ch == 'R') {
-                int rightTurns = (int) (ch == 'R' ? v : 360 - v) / 90;
-                for (int i = 0; i < rightTurns; i++) {
-                    long oldx = wx;
-                    wx = -1 * wy;
-                    wy = oldx;
-                }
+    private static class Position extends Vector {
+
+        public Position(long x, long y) {
+            super(x, y);
+        }
+
+        void move(char dir, int value) {
+            if (dir == 'N') {
+                y += value;
+            } else if (dir == 'S') {
+                y -= value;
+            } else if (dir == 'E') {
+                x += value;
+            } else if (dir == 'W') {
+                x -= value;
+            } else {
+                throw new IllegalArgumentException("Unknown direction for move: " + dir);
             }
         }
-        return Math.abs(x) + Math.abs(y);
+
+        void rotate(char dir, int degree) {
+            int rightTurns = (dir == 'R' ? degree : 360 - degree) / 90;
+            for (int i = 0; i < rightTurns; i++) {
+                rotateRight();
+            }
+        }
+
     }
 
 }
