101edda747d92c41acc96f495ffda2ea97faac1a
Péter Kovács <kovacspeter84@gmail.com>
2020-12-16 09:36:02 +0100

    Use Guava library (e.g. LongMath)

--- src/test/java/pkovacs/aoc/alg/NumericalAlgorithmsTest.java
+++ src/test/java/pkovacs/aoc/alg/NumericalAlgorithmsTest.java
@@ -1,6 +1,5 @@
 package pkovacs.aoc.alg;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,15 +11,9 @@ class NumericalAlgorithmsTest {
 
     @Test
     public void testIsPrime() {
+        assertFalse(NumericalAlgorithms.isPrime(0));
         assertFalse(NumericalAlgorithms.isPrime(1));
         assertTrue(NumericalAlgorithms.isPrime(2));
-        assertTrue(NumericalAlgorithms.isPrime(3));
-        assertFalse(NumericalAlgorithms.isPrime(4));
-        assertTrue(NumericalAlgorithms.isPrime(5));
-
-        assertTrue(NumericalAlgorithms.isPrime(41));
-        assertFalse(NumericalAlgorithms.isPrime(42));
-        assertTrue(NumericalAlgorithms.isPrime(43));
 
         assertTrue(NumericalAlgorithms.isPrime(7919));
         assertFalse(NumericalAlgorithms.isPrime(7919 * 5189));
@@ -32,31 +25,30 @@ class NumericalAlgorithmsTest {
         assertTrue(NumericalAlgorithms.isPrime(4398042316799L));
         assertTrue(NumericalAlgorithms.isPrime(688846502588399L));
         assertTrue(NumericalAlgorithms.isPrime(59604644783353249L));
-//        assertTrue(NumericalAlgorithms.isPrime(2305843009213693951L)); // 3-4s
-//        assertTrue(NumericalAlgorithms.isPrime(3318308475676071413L)); // 3-4s
-//        assertTrue(NumericalAlgorithms.isPrime(3331113965338635107L)); // 3-4s
+        assertTrue(NumericalAlgorithms.isPrime(2305843009213693951L));
+        assertTrue(NumericalAlgorithms.isPrime(3318308475676071413L));
+        assertTrue(NumericalAlgorithms.isPrime(3331113965338635107L));
         assertFalse(NumericalAlgorithms.isPrime(27644437L * 1073676287L));
 
-        assertThrows(IllegalArgumentException.class, () -> NumericalAlgorithms.isPrime(0));
         assertThrows(IllegalArgumentException.class, () -> NumericalAlgorithms.isPrime(-3));
     }
 
     @Test
     public void testGcd() {
-        assertEquals(1, NumericalAlgorithms.calculateGcd(6, 35));
-        assertEquals(3, NumericalAlgorithms.calculateGcd(15, 42));
+        assertEquals(1, NumericalAlgorithms.gcd(6, 35));
+        assertEquals(3, NumericalAlgorithms.gcd(15, 42));
         assertEquals(27644437L,
-                NumericalAlgorithms.calculateGcd(27644437L * 1073676287L, 27644437L * 7919 * 41));
+                NumericalAlgorithms.gcd(27644437L * 1073676287L, 27644437L * 7919 * 41));
         assertEquals(41,
-                NumericalAlgorithms.calculateGcd(3 * 41, 27644437L * 7919 * 41));
+                NumericalAlgorithms.gcd(3 * 41, 27644437L * 7919 * 41));
     }
 
     @Test
     public void testLcm() {
-        assertEquals(6 * 35, NumericalAlgorithms.calculateLcm(6, 35));
-        assertEquals(15 * 42 / 3, NumericalAlgorithms.calculateLcm(15, 42));
+        assertEquals(6 * 35, NumericalAlgorithms.lcm(6, 35));
+        assertEquals(15 * 42 / 3, NumericalAlgorithms.lcm(15, 42));
         assertEquals(1073676287L * 5189 * 7919 * 41,
-                NumericalAlgorithms.calculateLcm(1073676287L * 5189, 1073676287L * 7919 * 41));
+                NumericalAlgorithms.lcm(1073676287L * 5189, 1073676287L * 7919 * 41));
     }
 
 }
