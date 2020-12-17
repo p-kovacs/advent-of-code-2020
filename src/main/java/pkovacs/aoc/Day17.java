67f024ef8fd7b7007b6906b8c9f5906477922816
Péter Kovács <kovacspeter84@gmail.com>
2020-12-17 09:09:41 +0100

    Generalize solution for Day 17 using new util class Point

--- src/main/java/pkovacs/aoc/Day17.java
+++ src/main/java/pkovacs/aoc/Day17.java
@@ -1,6 +1,11 @@
 package pkovacs.aoc;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import pkovacs.aoc.util.InputUtils;
+import pkovacs.aoc.util.Point;
 
 public class Day17 {
 
@@ -8,118 +13,49 @@ public class Day17 {
         var input = InputUtils.readCharMatrix("day17.txt");
 
         int cycleCount = 6;
-        int n = input.length + 2 * (cycleCount + 2); // size of each dimension of the space
-
-        System.out.println("Part 1: " + solveDim3(input, n, cycleCount));
-        System.out.println("Part 2: " + solveDim4(input, n, cycleCount));
+        System.out.println("Part 1: " + solve(input, 3, cycleCount));
+        System.out.println("Part 2: " + solve(input, 4, cycleCount));
     }
 
-    private static long solveDim3(char[][] input, int n, int cycleCount) {
-        var space = new boolean[n][n][n];
+    private static long solve(char[][] input, int dim, int cycleCount) {
+        var actives = new HashSet<pkovacs.aoc.util.Point>();
 
-        int base = (n - input.length) / 2;
-        for (int x = 0; x < input.length; x++) {
-            for (int y = 0; y < input[x].length; y++) {
-                space[base + x][base + y][base] = input[y][x] == '#';
-            }
-        }
-
-        for (int cyc = 0; cyc < cycleCount; cyc++) {
-            var newSpace = new boolean[n][n][n];
-            for (int x = 1; x < space.length - 1; x++) {
-                for (int y = 1; y < space[x].length - 1; y++) {
-                    for (int z = 1; z < space[x][y].length - 1; z++) {
-                        int nc = countActiveNeighbors(space, x, y, z);
-                        newSpace[x][y][z] = (space[x][y][z] && (nc == 2 || nc == 3))
-                                || (!space[x][y][z] && nc == 3);
-                    }
-                }
-            }
-            space = newSpace;
-        }
-
-        return countActive(space);
-    }
-
-    private static long solveDim4(char[][] input, int n, int cycleCount) {
-        var space = new boolean[n][n][n][n];
-
-        int base = (n - input.length) / 2;
-        for (int x = 0; x < input.length; x++) {
-            for (int y = 0; y < input[x].length; y++) {
-                space[base + x][base + y][base][base] = input[y][x] == '#';
-            }
-        }
-
-        for (int cyc = 0; cyc < cycleCount; cyc++) {
-            var newSpace = new boolean[n][n][n][n];
-            for (int x = 1; x < space.length - 1; x++) {
-                for (int y = 1; y < space[x].length - 1; y++) {
-                    for (int z = 1; z < space[x][y].length - 1; z++) {
-                        for (int w = 1; w < space[x][y][z].length - 1; w++) {
-                            int nc = countActiveNeighbors(space, x, y, z, w);
-                            newSpace[x][y][z][w] = (space[x][y][z][w] && (nc == 2 || nc == 3))
-                                    || (!space[x][y][z][w] && nc == 3);
-                        }
-                    }
-                }
-            }
-            space = newSpace;
-        }
-
-        return countActive(space);
-    }
-
-    private static int countActiveNeighbors(boolean[][][] space, int x, int y, int z) {
-        int cnt = 0;
-        for (int xx = x - 1; xx <= x + 1; xx++) {
-            for (int yy = y - 1; yy <= y + 1; yy++) {
-                for (int zz = z - 1; zz <= z + 1; zz++) {
-                    if ((xx != x || yy != y || zz != z) && space[xx][yy][zz]) {
-                        cnt++;
-                    }
+        for (int row = 0; row < input.length; row++) {
+            for (int col = 0; col < input[row].length; col++) {
+                if (input[row][col] == '#') {
+                    actives.add(createPoint(dim, col, row));
                 }
             }
         }
-        return cnt;
+
+        for (int c = 0; c < cycleCount; c++) {
+            var newActives = actives.stream()
+                    .flatMap(p -> p.getNeighbors(true).stream())
+                    .distinct()
+                    .filter(p -> {
+                        boolean active = actives.contains(p);
+                        long nc = countActiveNeighbors(actives, p);
+                        return (active && (nc == 2 || nc == 3)) || (!active && nc == 3);
+                    })
+                    .collect(Collectors.toSet());
+            actives.clear();
+            actives.addAll(newActives);
+        }
+
+        return actives.size();
     }
 
-    private static int countActiveNeighbors(boolean[][][][] space, int x, int y, int z, int w) {
-        int cnt = 0;
-        for (int xx = x - 1; xx <= x + 1; xx++) {
-            for (int yy = y - 1; yy <= y + 1; yy++) {
-                for (int zz = z - 1; zz <= z + 1; zz++) {
-                    for (int ww = w - 1; ww <= w + 1; ww++) {
-                        if ((xx != x || yy != y || zz != z || ww != w) && space[xx][yy][zz][ww]) {
-                            cnt++;
-                        }
-                    }
-                }
-            }
-        }
-        return cnt;
+    private static Point createPoint(int dim, int x, int y) {
+        var coords = new int[dim];
+        coords[0] = x;
+        coords[1] = y;
+        return new Point(coords);
     }
 
-    private static int countActive(boolean[][][] space) {
-        int cnt = 0;
-        for (boolean[][] matrix : space) {
-            for (boolean[] array : matrix) {
-                for (boolean b : array) {
-                    if (b) {
-                        cnt++;
-                    }
-                }
-            }
-        }
-        return cnt;
-    }
-
-    private static int countActive(boolean[][][][] space) {
-        int cnt = 0;
-        for (boolean[][][] subspace : space) {
-            cnt += countActive(subspace);
-        }
-        return cnt;
+    private static long countActiveNeighbors(Set<Point> actives, Point point) {
+        return point.getNeighbors().stream()
+                .filter(actives::contains)
+                .count();
     }
 
 }
