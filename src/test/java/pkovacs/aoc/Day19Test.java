8e71a72515703fd1dc4119c46819680d412969df
Péter Kovács <kovacspeter84@gmail.com>
2020-12-19 07:34:50 +0100

    Add solution for Day 19

---
+++ src/test/java/pkovacs/aoc/Day19Test.java
@@ -0,0 +1,14 @@
+package pkovacs.aoc;
+
+import org.junit.jupiter.api.Test;
+
+class Day19Test extends DayTestBase {
+
+    @Test
+    public void test() {
+        Day19.main(null);
+        assertSolution1(107);
+        assertSolution2(321);
+    }
+
+}
