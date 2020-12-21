edb28cd33b8388e1ee31167c2372a3727b4d3637
Péter Kovács <kovacspeter84@gmail.com>
2020-12-21 08:59:05 +0100

    Add solution for Day 21

---
+++ src/test/java/pkovacs/aoc/Day21Test.java
@@ -0,0 +1,18 @@
+package pkovacs.aoc;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class Day21Test extends DayTestBase {
+
+    @Test
+    @Disabled("this time a string is required for part 2")
+    public void test() {
+//        Part 1: 1977
+//        Part 2: dpkvsdk,xmmpt,cxjqxbt,drbq,zmzq,mnrjrf,kjgl,rkcpxs
+        Day21.main(null);
+        assertSolution1(0);
+        assertSolution2(0);
+    }
+
+}
