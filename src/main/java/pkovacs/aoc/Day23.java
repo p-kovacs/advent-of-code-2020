a1cd79208618851aded2dc57ff8c42c6025f5e01
Péter Kovács <kovacspeter84@gmail.com>
2020-12-23 12:38:42 +0100

    Add solution for Day 23

---
+++ src/main/java/pkovacs/aoc/Day23.java
@@ -0,0 +1,98 @@
+package pkovacs.aoc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.primitives.Ints;
+import pkovacs.aoc.util.InputUtils;
+
+import static java.util.stream.Collectors.joining;
+
+public class Day23 {
+
+    public static void main(String[] args) {
+        var lines = InputUtils.readLines("day23.txt");
+
+        var list = new ArrayList<Integer>();
+        for (int i = 0; i < lines.get(0).length(); i++) {
+            list.add(Integer.parseInt(lines.get(0).substring(i, i + 1)));
+        }
+
+        System.out.println("Part 1: " + solve(list, false));
+        System.out.println("Part 2: " + solve(list, true));
+    }
+
+    private static String solve(List<Integer> input, boolean advanced) {
+        List<Integer> list = play(input, advanced);
+
+        if (advanced) {
+            return String.valueOf((long) list.get(0) * list.get(1));
+        } else {
+            return list.stream().map(String::valueOf).collect(joining(""));
+        }
+    }
+
+    private static List<Integer> play(List<Integer> input, boolean advanced) {
+        int moveCount = advanced ? 10_000_000 : 100;
+        int max = input.stream().mapToInt(a -> a).max().getAsInt();
+
+        // Extend list for part 2
+        var list = new ArrayList<>(input);
+        int n = advanced ? 1_000_000 : list.size();
+        if (advanced) {
+            for (int k = max + 1; k <= 1_000_000; k++) {
+                list.add(k);
+            }
+            max = 1_000_000;
+        }
+
+        // Build data structures. (Each value remains at the same position in values array, but their order
+        // is stored as a doubly-linked list, and a reverse lookup map is also stored in index array.)
+        int[] values = Ints.toArray(list);
+        int[] next = new int[n];
+        int[] prev = new int[n];
+        int[] index = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            next[i] = i == n - 1 ? 0 : i + 1;
+            prev[i] = i == 0 ? n - 1 : i - 1;
+            index[values[i]] = i;
+        }
+
+        // Play the moves
+        int current = 0;
+        for (int move = 0; move < moveCount; move++) {
+            int pu1 = next[current];
+            int pu2 = next[pu1];
+            int pu3 = next[pu2];
+
+            int currentValue = values[current];
+            int dest = currentValue;
+            while (dest == currentValue || dest == values[pu1] || dest == values[pu2] || dest == values[pu3]) {
+                if (--dest < 1) {
+                    dest = max;
+                }
+            }
+            int destIndex = index[dest];
+
+            int p = prev[pu1];
+            int q = next[pu3];
+            next[p] = q;
+            prev[q] = p;
+
+            prev[pu1] = destIndex;
+            next[pu3] = next[destIndex];
+            prev[next[destIndex]] = pu3;
+            next[destIndex] = pu1;
+
+            current = next[current];
+        }
+
+        // Construct result list
+        List<Integer> result = new ArrayList<>();
+        for (int i = next[index[1]], last = index[1]; i != last; i = next[i]) {
+            result.add(values[i]);
+        }
+        return result;
+    }
+
+}
