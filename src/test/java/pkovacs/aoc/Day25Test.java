0fb046ea8b0ea4a9f102a86cdfd05bfb127b7d1a
Péter Kovács <kovacspeter84@gmail.com>
2020-12-27 00:27:22 +0100

    Add solution for Day 25

---
+++ src/test/java/pkovacs/aoc/Day25Test.java
@@ -0,0 +1,14 @@
+package pkovacs.aoc;
+
+import org.junit.jupiter.api.Test;
+
+class Day25Test extends DayTestBase {
+
+    @Test
+    public void test() {
+        Day25.main(null);
+        assertSolution1("6408263");
+        assertSolution2("");
+    }
+
+}
