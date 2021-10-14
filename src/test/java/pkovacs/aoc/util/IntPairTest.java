c71bc3e947e89b5d1c2196af1829896273530303
Péter Kovács <kovacspeter84@gmail.com>
2021-10-14 16:31:25 +0200

    Refactoring: rename test class

--- src/test/java/pkovacs/aoc/util/IntPairTest.java
+++ src/test/java/pkovacs/aoc/util/IntPairTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-public class PairTest {
+public class IntPairTest {
 
     @Test
     public void testIntPair() {
